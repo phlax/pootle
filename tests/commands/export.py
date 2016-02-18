50458e64acd09fd58e5aa403c193a643cc0e3389
Dwayne Bailey <dwayne@translate.org.za>
2016-02-18 09:22:26 +0000

    Test: export add --path testing

--- tests/commands/export.py
+++ tests/commands/export.py
@@ -9,6 +9,7 @@
 import pytest
 
 from django.core.management import call_command
+from django.core.management.base import CommandError
 
 
 @pytest.mark.cmd
@@ -31,9 +32,52 @@ def test_export_project(capfd, afrikaans_tutorial, french_tutorial):
 
 @pytest.mark.cmd
 @pytest.mark.django_db
+def test_export_project_and_lang(capfd, afrikaans_tutorial, french_tutorial):
+    """Export a project TP"""
+    call_command('export', '--project=tutorial', '--language=af')
+    out, err = capfd.readouterr()
+    assert 'tutorial-af.zip' in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
 def test_export_language(capfd, afrikaans_tutorial, french_tutorial):
     """Export a language"""
     call_command('export', '--language=af')
     out, err = capfd.readouterr()
     assert 'af.zip' in out
     assert 'fr.zip' not in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_export_path(capfd, afrikaans_tutorial, french_tutorial):
+    """Export a path
+
+    Testing variants of lang, TP, single PO file and whole site.
+    """
+    call_command('export', '--path=/af')
+    out, err = capfd.readouterr()
+    assert 'af.zip' in out
+    assert 'fr.zip' not in out
+
+    call_command('export', '--path=/af/tutorial')
+    out, err = capfd.readouterr()
+    assert 'af-tutorial.zip' in out
+
+    call_command('export', '--path=/af/tutorial/tutorial.po')
+    out, err = capfd.readouterr()
+    assert 'tutorial.po' in out
+
+    call_command('export', '--path=/')
+    out, err = capfd.readouterr()
+    assert 'export.zip' in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_export_path_unknown():
+    """Export an unknown path"""
+    with pytest.raises(CommandError) as e:
+        call_command('export', '--path=/af/unknown')
+    assert "Could not find store matching '/af/unknown'" in str(e)
